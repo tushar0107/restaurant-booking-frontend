4692d22b5404a95e7acc873fd883867dde3db7ff
tushar0107 <tushardasare1701@gmail.com>
2024-04-07 18:16:18 +0530

    minor mistake fix

--- src/Vars.js
+++ src/Vars.js
@@ -1,5 +1,5 @@
-// const apiUrl = 'https://restaurant-booking-app-server.onrender.com'
-const apiUrl = 'http://127.0.0.1:8626';
+const apiUrl = 'https://restaurant-booking-app-server.onrender.com'
+// const apiUrl = 'http://127.0.0.1:8626';
 
 
 export default apiUrl;
